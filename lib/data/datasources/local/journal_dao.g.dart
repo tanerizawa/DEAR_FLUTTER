eb5b8fd7471b4213af8231635aabf517f106619d
tanerizawa <studiomalaka@gmail.com>
2025-06-29 00:15:01 +0700

    Remove generated and ignored files

--- lib/data/datasources/local/journal_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'journal_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$JournalDaoMixin on DatabaseAccessor<AppDatabase> {
-  $JournalEntriesTable get journalEntries => attachedDatabase.journalEntries;
-}
