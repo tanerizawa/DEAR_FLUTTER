f548668af0cf56058d443721d04ac7d5f5d00b17
tanerizawa <studiomalaka@gmail.com>
2025-06-29 00:18:29 +0700

    Delete Backend directory

--- Backend/app/crud/crud_article.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from .base import CRUDBase
-from app.models.article import Article
-from app.schemas.article import ArticleCreate, ArticleUpdate
-
-class CRUDArticle(CRUDBase[Article, ArticleCreate, ArticleUpdate]):
-    pass
-
-article = CRUDArticle(Article)
