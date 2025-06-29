83d62dcc370a1491c7db2b2ab78c24ea3c357ed9
tanerizawa <studiomalaka@gmail.com>
2025-06-29 09:46:33 +0700

    docs: document automatic base url switch

--- lib/core/di/register_module.dart
+++ lib/core/di/register_module.dart
@@ -1,8 +1,11 @@
+import 'dart:io' show Platform;
+
 import 'package:dear_flutter/core/api/auth_interceptor.dart';
 import 'package:dear_flutter/data/datasources/local/app_database.dart';
 import 'package:dear_flutter/data/datasources/local/chat_message_dao.dart';
 import 'package:dear_flutter/data/datasources/local/journal_dao.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,9 +14,15 @@ abstract class RegisterModule {
   // --- NETWORK ---
   @lazySingleton
   Dio dio(AuthInterceptor authInterceptor) {
+    final baseUrl = kIsWeb
+        ? 'http://localhost:8000/api/v1/'
+        : Platform.isAndroid
+            ? 'http://10.0.2.2:8000/api/v1/'
+            : 'http://localhost:8000/api/v1/';
+
     final dio = Dio(
       BaseOptions(
-        baseUrl: 'http://10.0.2.2:8000/api/v1/', // Untuk emulator
+        baseUrl: baseUrl,
         connectTimeout: const Duration(seconds: 30),
         receiveTimeout: const Duration(seconds: 60),
       ),
