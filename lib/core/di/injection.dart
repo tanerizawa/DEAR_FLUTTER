85fa7af2ed63be29f4466736ccb0ff14bf19d2a8
tanerizawa <studiomalaka@gmail.com>
2025-06-30 21:55:59 +0700

    fix(di): remove duplicate service registration

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -4,9 +4,6 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
 import 'injection.config.dart'; // File ini akan dibuat oleh generator
-import '../../services/notification_service.dart';
-import '../../services/quote_update_service.dart';
-import '../../data/datasources/remote/home_api_service.dart';
 
 final getIt = GetIt.instance;
 
@@ -17,8 +14,5 @@ final getIt = GetIt.instance;
 )
 Future<GetIt> configureDependencies() async {
   await getIt.init();
-  getIt.registerLazySingleton<NotificationService>(() => NotificationService());
-  getIt.registerLazySingleton<QuoteUpdateService>(
-      () => QuoteUpdateService(getIt<HomeApiService>(), getIt<NotificationService>()));
   return getIt;
 }
