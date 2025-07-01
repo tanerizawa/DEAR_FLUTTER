a6d3d15765197c2c317dfcf25390ec0245aa44f7
tanerizawa <studiomalaka@gmail.com>
2025-07-01 09:56:12 +0700

    feat(services): parse youtube manifest in isolate

--- test/youtube_audio_service_test.dart
+++ test/youtube_audio_service_test.dart
@@ -2,6 +2,7 @@ import 'package:dear_flutter/services/youtube_audio_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   test('returns highest bitrate url', () async {
     final service = YoutubeAudioService(fetcher: (id) async => [
       AudioInfo(128, Uri.parse('u1')),
