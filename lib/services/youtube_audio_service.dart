4948883b3362287485b2a4af31df5749a1b46138
tanerizawa <studiomalaka@gmail.com>
2025-07-02 19:10:16 +0700

    docs(service): add youtube audio service docs

--- lib/services/youtube_audio_service.dart
+++ lib/services/youtube_audio_service.dart
@@ -1,24 +1,48 @@
 import 'package:injectable/injectable.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
+/// Simple data holder representing an audio stream option from YouTube.
+///
+/// The [bitrate] is expressed in kbps and the [url] points to the audio
+/// content that can be streamed or downloaded.
 class AudioInfo {
+  /// Bitrate in kilobits per second of the audio stream.
   final int bitrate;
+
+  /// Direct URL to the audio file.
   final Uri url;
+
+  /// Creates an instance of [AudioInfo] with a given [bitrate] and [url].
   AudioInfo(this.bitrate, this.url);
 }
 
+/// Signature for a function that retrieves audio info for a YouTube video.
+///
+/// Implementations should return a list of available [AudioInfo] for the given
+/// video identifier.
 typedef AudioFetcher = Future<List<AudioInfo>> Function(String id);
 
 @injectable
+/// A small wrapper around [YoutubeExplode] to obtain audio-only streams.
 class YoutubeAudioService {
+  /// Client used to communicate with the YouTube API.
   final YoutubeExplode yt;
+
+  /// Optional custom fetcher used mainly for testing.
   final AudioFetcher? fetcher;
 
+  /// Creates an instance with an optional [fetcher] for dependency injection.
   YoutubeAudioService(this.yt, {@factoryParam this.fetcher});
 
+  /// Returns the URL to the highest bitrate audio-only stream of a video.
+  ///
+  /// [videoIdOrUrl] can be either the ID of a YouTube video or a full URL. The
+  /// function resolves available audio streams and picks the one with the
+  /// highest [AudioInfo.bitrate].
   Future<String> getAudioUrl(String videoIdOrUrl) async {
-    final infos =
-        fetcher != null ? await fetcher!(videoIdOrUrl) : await _fetch(id: videoIdOrUrl);
+    final infos = fetcher != null
+        ? await fetcher!(videoIdOrUrl)
+        : await _fetch(id: videoIdOrUrl);
     infos.sort((a, b) => a.bitrate.compareTo(b.bitrate));
     return infos.last.url.toString();
   }
@@ -30,5 +54,6 @@ class YoutubeAudioService {
         .toList();
   }
 
+  /// Closes the underlying [YoutubeExplode] client and frees resources.
   void close() => yt.close();
 }
